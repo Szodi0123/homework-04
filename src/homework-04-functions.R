c224ffadb77cde97e0da807f224195c497da8d20
Szodi0123 <adam.szilagyi.23@gmail.com>
2017-05-02 00:45:03 +0200

    Elkészült a két script. Kezdődhet a móka!

---
+++ src/homework-04-functions.R
@@ -0,0 +1,6 @@
+## Házi feladat 4
+## Programozás I.
+## 2016/17. II. félév
+## 2017.04.01.
+
+##### FUNCTIONS #####
---
+++ src/homework-04.R
@@ -0,0 +1,6 @@
+#I. feladat
+
+## Házi feladat 4
+## Programozás I. (SST111)
+## 2016/17. II. félév
+## 2017.05.01.
