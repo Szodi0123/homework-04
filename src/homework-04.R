0308bc9609e0968d66b07767a79e191d0a3a75e6
Szodi0123 <adam.szilagyi.23@gmail.com>
2017-05-07 16:18:10 +0200

    Kész van a IV.1 feladat.

--- src/homework-04.R
+++ src/homework-04.R
@@ -124,7 +124,7 @@ library(data.table)
 abra_1 <-
   melt(
     hiphop_cand_lyrics,
-    id.vars = c("album_release_date", "candidate","sentiment"),
+    id.vars = c("album_release_date", "candidate", "sentiment"),
     measure.vars = "candidate"
   )
 
@@ -132,14 +132,16 @@ abra_1 <-
 abra_1$freq <- 1
 
 #Ide helytakarékosság miatt elmentettem a neveket/vez.neveket/színkódokat.
-teljes_nevek <- c("Ben Carson",
-                  "Bernie Sanders",
-                  "Chris Christie",
-                  "Donald Trump",
-                  "Hillary Clinton",
-                  "Jeb Bush",
-                  "Mike Huckabee",
-                  "Ted Cruz")
+teljes_nevek <- c(
+  "Ben Carson",
+  "Bernie Sanders",
+  "Chris Christie",
+  "Donald Trump",
+  "Hillary Clinton",
+  "Jeb Bush",
+  "Mike Huckabee",
+  "Ted Cruz"
+)
 
 teljes_nevek == upcase_vez_nevek
 
@@ -152,29 +154,33 @@ upcase_vez_nevek <- c("CARSON",
                       "HUCKABEE",
                       "CRUZ")
 
-szinkodok <- c("#a7d65d",
-               "#6ac1a5",
-               "#fd8977",
-               "#fdbf7b",
-               "#96d7f8",
-               "#fed844",
-               "#fbcee5",
-               "#e58cc2")
+szinkodok <- c(
+  "#a7d65d",
+  "#6ac1a5",
+  "#fd8977",
+  "#fdbf7b",
+  "#96d7f8",
+  "#fed844",
+  "#fbcee5",
+  "#e58cc2"
+)
 
 
 require(ggplot2)
 
-ggplot(abra_1, aes(x = album_release_date,y= freq)) +
-  geom_point(aes(colour = abra_1$candidate),
+ggplot(abra_1, aes(x = album_release_date, y = freq)) +
+  geom_point(
+    aes(colour = abra_1$candidate),
     stat = "identity",
     size = 4,
-    position = position_stack(reverse=T)
+    position = position_stack(reverse = T)
   ) +
   scale_colour_manual(
     name = "",
     breaks = (teljes_nevek),
     labels = (upcase_vez_nevek),
-    values = (szinkodok)) +
+    values = (szinkodok)
+  ) +
   theme(legend.position = "top") + guides(colour = guide_legend(nrow = 1)) +
   theme(axis.title.x = element_blank()) +
   theme(axis.title.y = element_blank()) +
@@ -194,7 +200,7 @@ ggplot(abra_1, aes(x = album_release_date,y= freq)) +
     face = "bold",
     hjust = 0.5
   )) +
-  scale_x_continuous(breaks=c(1990,1995,2000,2005,2010,2015))
+  scale_x_continuous(breaks = c(1990, 1995, 2000, 2005, 2010, 2015))
 
 
 # Exportálás.
@@ -202,17 +208,17 @@ ggsave("/Users/Adam/homework-04/fig/hiphop1.png")
 
 
 #2. ábra: ugyanez a sentimentekre.
-positive <- subset(hiphop_cand_lyrics,sentiment == "positive")
-negative <- subset(hiphop_cand_lyrics,sentiment == "negative")
-neutral <- subset(hiphop_cand_lyrics,sentiment == "neutral")
+positive <- subset(hiphop_cand_lyrics, sentiment == "positive")
+negative <- subset(hiphop_cand_lyrics, sentiment == "negative")
+neutral <- subset(hiphop_cand_lyrics, sentiment == "neutral")
 
 
 #Ezeknél fontos, hogy a színeket nem tudom a vektorból megadni,
-#mert nem mindenkinek van sentimentes tweetje. 
+#mert nem mindenkinek van sentimentes tweetje.
 
 
 # Pozitív sentiment táblakezelés
-positive<-
+positive <-
   melt(
     positive,
     id.vars = c("album_release_date", "candidate", "sentiment"),
@@ -225,7 +231,7 @@ positive<-
 positive$freq <- 1
 
 #Ábrázolás
-pos <- ggplot(positive, aes(x = album_release_date, y =freq)) +
+pos <- ggplot(positive, aes(x = album_release_date, y = freq)) +
   geom_point(
     aes(colour = positive$candidate),
     stat = "identity",
@@ -236,15 +242,19 @@ pos <- ggplot(positive, aes(x = album_release_date, y =freq)) +
     name = "",
     breaks = (teljes_nevek),
     labels = (upcase_vez_nevek),
-    values = ( values = c(
-      "Ben Carson" = "#a7d65d",
-      "Bernie Sanders" = "#6ac1a5",
-      "Chris Christie" = "#6ac1a5",
-      "Donald Trump" = "#fdbf7b",
-      "Hillary Clinton" = "#96d7f8",
-      "Jeb Bush" = "#fed844",
-      "Mike Huckabee" = "#fbcee5",
-      "Ted Cruz" = "#e58cc2"))) +
+    values = (
+      values = c(
+        "Ben Carson" = "#a7d65d",
+        "Bernie Sanders" = "#6ac1a5",
+        "Chris Christie" = "#6ac1a5",
+        "Donald Trump" = "#fdbf7b",
+        "Hillary Clinton" = "#96d7f8",
+        "Jeb Bush" = "#fed844",
+        "Mike Huckabee" = "#fbcee5",
+        "Ted Cruz" = "#e58cc2"
+      )
+    )
+  ) +
   theme(legend.position = "bottom") + guides(colour = guide_legend(nrow = 1)) +
   theme(axis.title.x = element_blank()) +
   theme(axis.title.y = element_blank()) +
@@ -264,10 +274,10 @@ pos <- ggplot(positive, aes(x = album_release_date, y =freq)) +
     face = "bold",
     hjust = 0.5
   )) +
-  scale_x_continuous(breaks=c(1990,1995,2000,2005,2010,2015))
+  scale_x_continuous(breaks = c(1990, 1995, 2000, 2005, 2010, 2015))
 
 # Negatív sentiment
-negative<-
+negative <-
   melt(
     negative,
     id.vars = c("album_release_date", "candidate", "sentiment"),
@@ -279,7 +289,7 @@ negative<-
 negative$freq <- 1
 
 
-neg <- ggplot(negative, aes(x = album_release_date, y= freq)) +
+neg <- ggplot(negative, aes(x = album_release_date, y = freq)) +
   geom_point(
     aes(colour = negative$candidate),
     stat = "identity",
@@ -290,15 +300,19 @@ neg <- ggplot(negative, aes(x = album_release_date, y= freq)) +
     name = "",
     breaks = (teljes_nevek),
     labels = (upcase_vez_nevek),
-    values = ( values = c(
-      "Ben Carson" = "#a7d65d",
-      "Bernie Sanders" = "#6ac1a5",
-      "Chris Christie" = "#6ac1a5",
-      "Donald Trump" = "#fdbf7b",
-      "Hillary Clinton" = "#96d7f8",
-      "Jeb Bush" = "#fed844",
-      "Mike Huckabee" = "#fbcee5",
-      "Ted Cruz" = "#e58cc2"))) +
+    values = (
+      values = c(
+        "Ben Carson" = "#a7d65d",
+        "Bernie Sanders" = "#6ac1a5",
+        "Chris Christie" = "#6ac1a5",
+        "Donald Trump" = "#fdbf7b",
+        "Hillary Clinton" = "#96d7f8",
+        "Jeb Bush" = "#fed844",
+        "Mike Huckabee" = "#fbcee5",
+        "Ted Cruz" = "#e58cc2"
+      )
+    )
+  ) +
   theme(legend.position = "bottom") + guides(colour = guide_legend(nrow = 1)) +
   theme(axis.title.x = element_blank()) +
   theme(axis.title.y = element_blank()) +
@@ -318,11 +332,11 @@ neg <- ggplot(negative, aes(x = album_release_date, y= freq)) +
     face = "bold",
     hjust = 0.5
   )) +
-  scale_x_continuous(breaks=c(1990,1995,2000,2005,2010,2015))
+  scale_x_continuous(breaks = c(1990, 1995, 2000, 2005, 2010, 2015))
 
 
 # Neutral sentiment
-neutral<-
+neutral <-
   melt(
     neutral,
     id.vars = c("album_release_date", "candidate", "sentiment"),
@@ -331,7 +345,7 @@ neutral<-
 
 neutral$freq <- 1
 
-neut <- ggplot(neutral, aes(x = album_release_date, y =freq)) +
+neut <- ggplot(neutral, aes(x = album_release_date, y = freq)) +
   geom_point(
     aes(colour = neutral$candidate),
     stat = "identity",
@@ -342,15 +356,19 @@ neut <- ggplot(neutral, aes(x = album_release_date, y =freq)) +
     name = "",
     breaks = (teljes_nevek),
     labels = (upcase_vez_nevek),
-    values = ( values = c(
-      "Ben Carson" = "#a7d65d",
-      "Bernie Sanders" = "#6ac1a5",
-      "Chris Christie" = "#6ac1a5",
-      "Donald Trump" = "#fdbf7b",
-      "Hillary Clinton" = "#96d7f8",
-      "Jeb Bush" = "#fed844",
-      "Mike Huckabee" = "#fbcee5",
-      "Ted Cruz" = "#e58cc2"))) +
+    values = (
+      values = c(
+        "Ben Carson" = "#a7d65d",
+        "Bernie Sanders" = "#6ac1a5",
+        "Chris Christie" = "#6ac1a5",
+        "Donald Trump" = "#fdbf7b",
+        "Hillary Clinton" = "#96d7f8",
+        "Jeb Bush" = "#fed844",
+        "Mike Huckabee" = "#fbcee5",
+        "Ted Cruz" = "#e58cc2"
+      )
+    )
+  ) +
   theme(legend.position = "bottom") + guides(colour = guide_legend(nrow = 1)) +
   theme(axis.title.x = element_blank()) +
   theme(axis.title.y = element_blank()) +
@@ -370,56 +388,279 @@ neut <- ggplot(neutral, aes(x = album_release_date, y =freq)) +
     face = "bold",
     hjust = 0.5
   )) +
-  scale_x_continuous(breaks=c(1990,1995,2000,2005,2010,2015))
+  scale_x_continuous(breaks = c(1990, 1995, 2000, 2005, 2010, 2015))
 
 
 library(grid)
 library(gridExtra)
-osszefuzott <- grid.arrange(pos,neg,neut,nrow=1,
-                            top="Candidate mentions, by sentiment")
+osszefuzott <- grid.arrange(pos, neg, neut, nrow = 1,
+                            top = "Candidate mentions, by sentiment")
 
 # Exportálás.
-ggsave("/Users/Adam/homework-04/fig/hiphop2.png", plot=osszefuzott,
-       width = 15.4, height = 5.82)
+ggsave(
+  "/Users/Adam/homework-04/fig/hiphop2.png",
+  plot = osszefuzott,
+  width = 15.4,
+  height = 5.82
+)
 
 
 #3.2 Egyéni ábra elkészítése
 #Itt arra gondoltam, hogy megnézem, hogy a két elnökjelöltnél a pénz témájú
 #dalszövegeken belül hogyan oszlik meg a dalszövegek szentimentje.
 
-sajat_abra <- subset(hiphop_cand_lyrics,theme == "money")
+sajat_abra <- subset(hiphop_cand_lyrics, theme == "money")
 
 
 #Ábrázoláshoz előkészítés. Itt a sentiment és a candidate szerint csoportosítok
 #majd leszámláltatom vele külön, hogy mennyi az összes és a perc oszlopba
-#százalékot számoltatok. Így kerültem meg, hogy különböző mennyiségű 
+#százalékot számoltatok. Így kerültem meg, hogy különböző mennyiségű
 #money-s tweetjük van (Trumpnak sokkal több van).
 library(dplyr)
-sajat_abra <- sajat_abra %>% 
-  group_by(candidate, sentiment) %>% 
-  summarise(count=n()) %>% 
-  mutate(perc=count/sum(count))
+sajat_abra <- sajat_abra %>%
+  group_by(candidate, sentiment) %>%
+  summarise(count = n()) %>%
+  mutate(perc = count / sum(count))
 
 #Nem törődtem sokat a külalakkal, inkább az ábra a fontos
-ggplot(data = sajat_abra2, aes(x=factor(candidate), y= perc*100, 
-                               fill=sentiment)) +
+ggplot(data = sajat_abra2, aes(
+  x = factor(candidate),
+  y = perc * 100,
+  fill = sentiment
+)) +
   geom_bar(stat = "identity", position = position_dodge()) +
   theme(plot.title = element_text(hjust = 0.59)) +
   labs(title = "A pénz témájú dalszövegek érzelmi töltete, jelöltenként",
-       y = "(%)", x="") +
-  scale_fill_manual(name = "Érzelmi töltet",
-                    breaks = c("negative", "neutral", "positive"),
-                    labels = c("Negatív", "Semleges", "Pozitv"), 
-                    values= c("aquamarine3", "bisque3", "coral3"))
+       y = "(%)", x = "") +
+  scale_fill_manual(
+    name = "Érzelmi töltet",
+    breaks = c("negative", "neutral", "positive"),
+    labels = c("Negatív", "Semleges", "Pozitv"),
+    values = c("aquamarine3", "bisque3", "coral3")
+  )
 
 
-#Innen pedig jól látszik, hogy valahogy a pozitívak vannak többségben. 
-#Bár ez egy kicsit azért fura. 
+#Innen pedig jól látszik, hogy valahogy a pozitívak vannak többségben.
+#Bár ez egy kicsit azért fura.
 
 ggsave("/Users/Adam/homework-04/fig/hiphop3.png")
 
 
-#IV. feladat
+#IV.1 feladat I.rész: ábrázolások
+#Tulajdonképpen ugyanazt kell megcsinálnom a tweetsre,
+#mint a saját ábránál, csak nem kell szűrnöm csoportra.
+tweets <-
+  read.table(file = "data/clinton_trump_tweets.csv", sep = ";", header = T)
+
+emo_tweets <- tweets %>%
+  group_by(handle, text_emotion) %>%
+  summarise(count = n()) %>%
+  mutate(perc = count / sum(count))
+
+senti_tweets <- tweets %>%
+  group_by(handle, text_sentiment) %>%
+  summarise(count = n()) %>%
+  mutate(perc = count / sum(count))
+
+
+#Ábrázolás: emotion_text
+plot1 <-
+  ggplot(data = emo_tweets, aes(
+    x = factor(handle),
+    y = perc * 100,
+    fill = text_emotion
+  )) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  theme(plot.title = element_text(hjust = 0.59)) +
+  labs(title = "A jelöltek tweetjei érzelmi besorolás alapján",
+       y = "(%)", x = "") +
+  scale_fill_manual(
+    name = "Érzelmek",
+    values = szinkodok,
+    label = c(
+      "harag",
+      "undor",
+      "félelem",
+      "öröm",
+      "szomorúság",
+      "meglepettség",
+      "nem felismerhető"
+    )
+  ) + scale_x_discrete(
+    breaks = c("HillaryClinton", "realDonaldTrump"),
+    labels = c("Hillary Clinton", "Donald Trump")
+  )
+
+#Ábrázolása: sentiment_text
+plot2 <-
+  ggplot(data = senti_tweets, aes(
+    x = factor(handle),
+    y = perc * 100,
+    fill = text_sentiment
+  )) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  theme(plot.title = element_text(hjust = 0.59)) +
+  labs(title = "A jelöltek tweetjei szentimentek alapján",
+       y = "(%)", x = "") +
+  scale_fill_manual(
+    name = "Érzelmi töltet (sentiment)",
+    values = szinkodok,
+    label = c("Negatív", "Semleges", "Pozitív")
+  ) + scale_x_discrete(
+    breaks = c("HillaryClinton", "realDonaldTrump"),
+    labels = c("Hillary Clinton", "Donald Trump")
+  )
+
+#Összevont ábra I.
+grid.arrange(plot1, plot2, nrow = 1, top = "Összehasonlítás")
+
+#IV.1 feladat II.rész: időSOROS ábrázolások
+#Beállítom, hogy csak a hónapokat mutassa, és aszerint sorba rendezem.
+tweets$time <- as.Date(tweets$time)
+tweets_sorted <- tweets[order(tweets[, 6]),]
+tweets_sorted$months <- strftime(tweets_sorted$time, format = "%m")
+
+#Mivel úgy szeretném, hogy legyen külön árbán legyen az emotion elemzés
+#jelöltenként, meg kell csinálnom, egy sentiment/emotion szerinti táblát
+#majd külön subseteket külön ábrákon ábrázolni. Ezután  majd összevonom a
+#tipológia szerint.
+
+#Idősoros-emotion
+idosorhoz_emot <- tweets_sorted %>%
+  group_by(handle, months, text_emotion) %>%
+  summarise(count = n()) %>%
+  mutate(perc = count / sum(count))
+
+idosor_emot_trump <-
+  subset(idosorhoz_emot, handle == "realDonaldTrump")
+idosor_emot_clinton <-
+  subset(idosorhoz_emot, handle == "HillaryClinton")
+
+p_idosor_emot_1 <-
+  ggplot(data = idosor_emot_trump,
+         aes(
+           x = months,
+           y = perc * 100,
+           fill = text_emotion,
+           show.legend
+         )) +
+  geom_bar(stat = "identity",
+           position = position_stack(),
+           show.legend = F) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Trump",
+       y = "(%)", x = "") +
+  scale_fill_manual(
+    name = "Érzelmek",
+    values = szinkodok,
+    label = c(
+      "harag",
+      "undor",
+      "félelem",
+      "öröm",
+      "szomorúság",
+      "meglepettség",
+      "nem felismerhető"
+    )
+  )
+
+
+p_idosor_emot_2 <-
+  ggplot(data = idosor_emot_clinton,
+         aes(
+           x = months,
+           y = perc * 100,
+           fill = text_emotion,
+           show.legend
+         )) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Clinton",
+       y = "(%)", x = "") +
+  scale_fill_manual(
+    name = "Érzelmek",
+    values = szinkodok,
+    label = c(
+      "harag",
+      "undor",
+      "félelem",
+      "öröm",
+      "szomorúság",
+      "meglepettség",
+      "nem felismerhető"
+    )
+  )
+
+
+#Összevont ábra II.1 emotion
+p_idosor_emot <-
+  grid.arrange(p_idosor_emot_1,
+               p_idosor_emot_2,
+               nrow = 1,
+               top = "A jelöltek tweetjei érzelmi besorolás alapján")
+
+#Idősoros: sentiment
+idosorhoz_sent <- tweets_sorted %>%
+  group_by(handle, months, text_sentiment) %>%
+  summarise(count = n()) %>%
+  mutate(perc = count / sum(count))
+
+idosor_sent_trump <-
+  subset(idosorhoz_sent, handle == "realDonaldTrump")
+idosor_sent_clinton <-
+  subset(idosorhoz_sent, handle == "HillaryClinton")
+
+p_idosor_sent_1 <-
+  ggplot(data = idosor_sent_trump,
+         aes(
+           x = months,
+           y = perc * 100,
+           fill = text_sentiment,
+           show.legend
+         )) +
+  geom_bar(stat = "identity",
+           position = position_stack(),
+           show.legend = F) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Trump",
+       y = "(%)", x = "") +
+  scale_fill_manual(
+    name = "Érzelmi töltet (sentiment)",
+    values = szinkodok,
+    label = c("Negatív", "Semleges", "Pozitív")
+  )
+
+p_idosor_sent_2 <-
+  ggplot(data = idosor_sent_clinton, aes(x = months, y = perc * 100,
+                                         fill = text_sentiment)) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Clinton",
+       y = "(%)", x = "") +
+  scale_fill_manual(
+    name = "Érzelmi töltet (sentiment)",
+    values = szinkodok,
+    label = c("Negatív", "Semleges", "Pozitív")
+  )
+
+
+#Összevont ábra II.2 sentiment
+p_idosor_sent <-
+  grid.arrange(p_idosor_sent_1,
+               p_idosor_sent_2,
+               nrow = 1,
+               top = "A jelöltek tweetjei sentiment besorolás alapján")
+
+
+#IV.1 feladat: 3. rész: próba
+chisq.test(x=tweets$handle, tweets$text_sentiment)
+chisq.test(x=tweets$handle, tweets$text_emotion)
+#Ezek szerint mind a szentiment, mind pedig az alapérzelmes 
+#beosztás mentén szig.különbség van.
+
+
+
+
 
 
 
